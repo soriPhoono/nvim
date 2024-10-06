1ac1b343885dedbdb1140b084a244d9b532df801
soriphoono <soriphoono@gmail.com>
2024-10-06 16:12:30 -0500

    Attempt 2 to enable lsp

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,19 +1,15 @@
-vim.bo.comments = ':#'
-
-local py_ls_cmd = 'pylsp'
-
-if vim.fn.executable(py_ls_cmd) ~= 1 then
+if vim.fn.executable('pylsp') ~= 1 then
   return
 end
 
 local root_files = {
-  '.git',
   'pyproject.toml',
+  '.git',
 }
 
 vim.lsp.start {
   name = 'pylsp',
-  cmd = { py_ls_cmd },
+  cmd = { 'pylsp' },
   root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
