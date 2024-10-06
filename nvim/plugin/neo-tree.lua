8f5614df2686a24dae631bf734a9ac64aaccda2c
soriphoono <soriphoono@gmail.com>
2024-10-05 21:08:49 -0500

    Updated neo-tree to hijack netrw windows

--- nvim/plugin/neo-tree.lua
+++ nvim/plugin/neo-tree.lua
@@ -1,14 +1,23 @@
 local fn = vim.fn
+local keymap = vim.keymap
 
 fn.sign_define("DiagnosticSignError",
-  { text = " ", texthl = "DiagnosticSignError"})
+  { text = " ", texthl = "DiagnosticSignError" })
 fn.sign_define("DiagnosticSignWarn",
-        {text = " ", texthl = "DiagnosticSignWarn"})
+  { text = " ", texthl = "DiagnosticSignWarn" })
 fn.sign_define("DiagnosticSignInfo",
-        {text = " ", texthl = "DiagnosticSignInfo"})
+  { text = " ", texthl = "DiagnosticSignInfo" })
 fn.sign_define("DiagnosticSignHint",
-        {text = "󰌵", texthl = "DiagnosticSignHint"})
+  { text = "󰌵", texthl = "DiagnosticSignHint" })
+
+keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>', { silent = true, desc = "Open file explorer" })
 
 require('neo-tree').setup({
   close_if_last_window = true,
+
+  filesystem = {
+    window = {
+      position = 'current',
+    }
+  }
 })
