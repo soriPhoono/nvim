aaf7d439fb4246c8202c72f34c2ab8a5706f0bed
soriphoono <soriphoono@gmail.com>
2024-10-07 13:00:37 -0500

    Updated toml plugin

--- nvim/ftplugin/toml.lua
+++ nvim/ftplugin/toml.lua
@@ -2,13 +2,13 @@ if vim.fn.executable('taplo') ~= 1 then
   return
 end
 
-local root_files = {
-  '*.toml'
-}
+local pattern = '.*.toml$'
 
 vim.lsp.start {
   name = 'taplo',
   cmd = { 'taplo lsp stdio' },
-  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  root_dir = vim.fs.dirname(vim.fs.find(function (name, path)
+    return name:match(pattern)
+  end, { upward = true })[1]),
   capabilities = require('user.lsp').make_client_capabilities(),
 }
