3fbfda56ce878ad537bf5644baed1ba2278680a2
soriphoono <soriphoono@gmail.com>
2024-10-08 13:24:04 -0500

    Updated nix lsp

--- nvim/ftplugin/nix.lua
+++ nvim/ftplugin/nix.lua
@@ -1,1 +1,28 @@
-require('lspconfig').nil_ls.setup({})
+vim.bo.comments = ':#'
+
+local nil_ls_cmd = 'nil'
+
+if vim.fn.executable(nil_ls_cmd) ~= 1 then
+  return
+end
+
+local root_files = {
+  'flake.nix',
+  'shell.nix',
+  'default.nix',
+  '.git'
+}
+
+vim.lsp.start {
+  name = 'nil_ls',
+  cmd = { nil_ls_cmd },
+  root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    ["nil"] = {
+      formatting = {
+        command = { 'nixpkgs-fmt' },
+      },
+    },
+  },
+}
