aed88761c1dc647e0ac5da4a8b5d65f0b0ad4150
soriphoono <soriphoono@gmail.com>
2024-10-10 13:49:29 -0500

    Moved lspconfig setups

--- nvim/plugin/lspconfig.lua
+++ nvim/plugin/lspconfig.lua
@@ -1,5 +1,50 @@
 local lspconfig = require('lspconfig')
 
+lspconfig.nixd.setup {
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    nixd = {
+      formatting = {
+        command = { "nixpkgs-fmt" },
+      },
+    },
+  },
+}
+
 lspconfig.bashls.setup {
   capabilities = require('user.lsp').make_client_capabilities(),
 }
+
+lspconfig.lua_ls.setup {
+  capabilities = require('user.lsp').make_client_capabilities(),
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global, etc.
+        globals = {
+          'vim',
+          'describe',
+          'it',
+          'assert',
+          'stub',
+        },
+        disable = {
+          'duplicate-set-field',
+        },
+      },
+      workspace = {
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+      hint = { -- inlay hints (supported in Neovim >= 0.10)
+enable = true,
+},
+    },
+  },
+}
+
