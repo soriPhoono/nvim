ea83421c1f60c3bb61c07c076d22124e0186e649
soriphoono <soriphoono@gmail.com>
2024-12-15 14:15:22 -0600

    Updated plugins

--- nix/modules/home/nvim/plugins/util.nix
+++ nix/modules/home/nvim/plugins/util.nix
@@ -1,6 +1,12 @@
 {
   programs.nixvim.plugins = {
     web-devicons.enable = true;
+    nvim-autopairs.enable = true;
     gitsigns.enable = true;
+
+    which-key = {
+      enable = true;
+      settings.preset = "helix";
+    };
   };
 }
