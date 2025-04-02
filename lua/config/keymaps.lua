f743d288af5bc6f73613ec196a6a6168f8de16ef
soriphoono <soriphoono@gmail.com>
2025-04-02 13:46:17 -0500

    Updated neovim ui

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,5 @@
 -- Sane defaults
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
-
 -- Terminal
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
